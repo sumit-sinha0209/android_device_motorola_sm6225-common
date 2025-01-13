2871f129fa1a9da1b23ce003da059fa7d4ba8464
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sm6225-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- Android.mk
+++ Android.mk
@@ -7,28 +7,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter borneo capri caprip cebu guam guamna guamp devon hawao rhode, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under, $(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-
-FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
-$(FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
-
-BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
-$(BT_FIRMWARE_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/bt_firmware
-
-DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
-$(DSP_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating $(DSP_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
-
-FSG_MOUNT_POINT := $(TARGET_OUT_VENDOR)/fsg
-$(FSG_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating $(FSG_MOUNT_POINT)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/fsg
-
-ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(BT_FIRMWARE_MOUNT_POINT) $(DSP_MOUNT_POINT) $(FSG_MOUNT_POINT)
 endif
--- bengal.mk
+++ bengal.mk
@@ -215,6 +215,12 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_fsg_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Permissions
