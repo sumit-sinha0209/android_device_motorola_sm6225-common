d2bd254fe992f59cafdc0905265a61e197b8a99a
Michael Bestas <mkbestas@lineageos.org>
2025-09-04 19:17:55 +0300

    sm6225-common: Build LiveDisplay SDM HAL
    Change-Id: I0e403c9d7455cba1d356e4901a4dd4184d483f5b

--- common.mk
+++ common.mk
@@ -200,6 +200,12 @@ PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+
 # Media
 PRODUCT_PACKAGES += \
     libOmxCore \
