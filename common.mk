8382890b29b0f137c730fb01effdd2717143c5b2
Michael Bestas <mkbestas@lineageos.org>
2025-09-05 12:38:38 +0300

    sm6225-common: Build LiveDisplay sysfs HAL
    Change-Id: I7c5bd2cb2eaa94ce789d8c4444eff93e2ad7d8a1

--- common.mk
+++ common.mk
@@ -202,9 +202,11 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.sdm
+    vendor.lineage.livedisplay-service.sdm \
+    vendor.lineage.livedisplay-service.sysfs
 
 $(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
 # Media
 PRODUCT_PACKAGES += \
