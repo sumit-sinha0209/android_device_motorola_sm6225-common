5618fe86823099d35c3388deeacd157dae87748c
Michael Bestas <mkbestas@lineageos.org>
2025-10-12 22:27:38 -0300

    sm6225-common: overlay: Convert overlays to Android.bp Change-Id: I5c54c2b133cb94afda1c06a941ed5a80e4d82361

--- common.mk
+++ common.mk
@@ -237,10 +237,18 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+
+PRODUCT_PACKAGES += \
+    FrameworksResCommon \
+    FrameworksResProduct \
+    FrameworksResTarget \
+    NcmTetheringOverlay \
+    SystemUIResCommon \
+    TelephonyResProduct \
+    WifiResCommon
 
 # Partitions
 PRODUCT_PACKAGES += \
@@ -362,9 +370,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     wpa_supplicant \
     wpa_supplicant.conf
-
-PRODUCT_PACKAGES += \
-    WifiResCommonOverlay
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
